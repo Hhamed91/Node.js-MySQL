3c1258f05a0fd06525da4c062cc09b5757946509
user.name <hazem.hamed.h@hotmail.com>
2019-09-23 16:33:21 -0700

    set up the inquirer prompt and defin the variables for the answers

--- BamazonCustomer.js
+++ BamazonCustomer.js
@@ -23,7 +23,38 @@ connection.query('SELECT * FROM Products', function(err, res){
     console.log('--------------------------------------------------------------------------------------------------')
   }
 
-  
+  console.log(' ');
+  inquirer.prompt([
+    {
+      type: "input",
+      name: "id",
+      message: "What is the ID of the product you would like to purchase?",
+      validate: function(value){
+        if(isNaN(value) == false && parseInt(value) <= res.length && parseInt(value) > 0){
+          return true;
+        } else{
+          return false;
+        }
+      }
+    },
+    {
+      type: "input",
+      name: "qty",
+      message: "How much would you like to purchase?",
+      validate: function(value){
+        if(isNaN(value)){
+          return false;
+        } else{
+          return true;
+        }
+      }
+    }
+    ]).then(function(ans){
+      var whatToBuy = (ans.id)-1;
+      var howMuchToBuy = parseInt(ans.qty);
+      var grandTotal = parseFloat(((res[whatToBuy].Price)*howMuchToBuy).toFixed(2));
+
+     
 })
 }
 
